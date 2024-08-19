76e8db45d29d976cdd17a9e1819e7d7c5dc61e33
jdmax15 <joel.maxwell15@gmail.com>
2024-08-19 15:19:31 +0930

    fixed board up, main still not working as intended

--- slot_machine.py
+++ slot_machine.py
@@ -26,11 +26,20 @@ class Slot_Machine:
 
         return result_1, result_2, result_3
     
-
+# NEED TO FIX SPIN/BETTING LOGIC. IT CURRENTLY ONLY CHANGES MONEY IF YOU PRESS 1 AND KEEPS RESPINNING.
 def main():
     slots = Slot_Machine(100)
-    r1, r2, r3 = slots.spin_result
-    slots.make_board(r1, r2, r3)
+    while True:
+        r1, r2, r3 = slots.spin()
+        slots.make_board(r1, r2, r3)
+        try:
+            choice = input("Type 1 to spin.")
+            slots.total -= 1
+            if choice == "1" and r1 == r2 and r1 == r3:
+                slots.total += 5
+
+        except ValueError:
+            print("Please only enter 1.")
 
 
 if __name__ == "__main__":
