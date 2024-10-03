7c21a925f27e413bf59d4d94289e561935084ae1
jdmax15 <joel.maxwell15@gmail.com>
2024-10-04 04:55:17 +0930

    fixed betting input

--- slot_machine.py
+++ slot_machine.py
@@ -40,8 +40,9 @@ class Slot_Machine:
             self.total += bet * 5
         self.make_board(r1, r2, r3)   
 
-# GettingUnboundLocalError with start_money
 def main():
+    start_money = 100
+    
     try:
         start_money = input(int("Enter your starting money: "))
     except ValueError:
@@ -52,9 +53,10 @@ def main():
     while True:
         try:
             choice = int(input("Type amount to bet (1-5). "))
-            if choice not in list(range(1, 5)):
+            if choice not in list(range(1, 6)):
                 raise ValueError()
-            slots.set_bet_and_spin(choice)
+            else:
+                slots.set_bet_and_spin(choice)
 
         except ValueError:
             print("Please only enter 1-5.")
